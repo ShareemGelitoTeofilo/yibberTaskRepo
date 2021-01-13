a3c8b778c0ef48a4579d4c70cd810b37ed0fd225
LAPTOP-9D7D05BF\Shareem <shareemgelitoteofilo@yahoo.com>
2021-01-13 18:21:45 +0800

    Updated all fragments to have data

--- app/src/main/java/com/example/yibbertask/activities/main/fragments/FragmentTagged.java
+++ app/src/main/java/com/example/yibbertask/activities/main/fragments/FragmentTagged.java
@@ -3,12 +3,18 @@ package com.example.yibbertask.activities.main.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.yibbertask.R;
+import com.example.yibbertask.activities.main.RecyclerViewAdapter;
+import com.example.yibbertask.models.Feed;
+
+import java.util.List;
 
 public class FragmentTagged extends Fragment {
 
@@ -17,6 +23,12 @@ public class FragmentTagged extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_tagged, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerViewPosts);
+
+        List<Feed> feeds = Feed.createRecords();
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(feeds);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         return view;
     }
 }
